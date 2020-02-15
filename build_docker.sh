7f60f2a6686f60cf13a50fbd1f8ac4f0acb4cc11
sheaffej <sheaffej@gmail.com>
2020-02-15 14:27:43 -0800

    add Docker container for local testing

---
+++ build_docker.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+DOCKER_IMAGE="sheaffej/roboclaw_driver"
+
+docker build -t $DOCKER_IMAGE .
---
+++ run/tests.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+DOCKER_IMAGE="sheaffej/roboclaw_driver"
+
+# [ -z "$ROS_MASTER_URI" ] && echo "Please set ROS_MASTER_URI env" && exit 1
+
+# MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# REPOS_DIR=$MYDIR/../..  # Directory containing the cloned git repos
+# CODE_MOUNT="/workspaces"
+
+docker run -it --rm \
+--privileged \
+--net host \
+--env DISPLAY \
+${DOCKER_IMAGE} /ros/src/roboclaw_driver/tests/run_tests.sh
+
+# --env ROS_MASTER_URI \
